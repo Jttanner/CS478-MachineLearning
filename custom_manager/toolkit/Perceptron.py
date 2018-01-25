3187a53740afb56e45323ca8701fafe97cfee572
Jttanner <jonttanner@gmail.com>
2018-01-25 10:05:57 -0700

    added basic datasets, small binary pereceptron works.  Learns the non-linearly seperable data set as best it can.

--- custom_manager/toolkit/Perceptron.py
+++ custom_manager/toolkit/Perceptron.py
@@ -4,8 +4,8 @@ from supervised_learner import SupervisedLearner
 from matrix import Matrix
 
 class Perceptron():
-    def __init__(self, type):
-        self.weights = [0] * 5
+    def __init__(self, type, inputSize):
+        self.weights = [0] * (inputSize + 1)
         self.type = type
 
     type = 0
@@ -26,7 +26,7 @@ class Perceptron():
     def computeNet(self, input):
         #input.append(1)
         sum = 0
-        for i in range(0,5):
+        for i in range(0,len(input)):
             sum += input[i] * self.weights[i]
         return sum;
 
@@ -36,6 +36,25 @@ class Perceptron():
         else:
             return 0
 
+    def copyList(self, original, destination):
+        for i in range(0, len(original)):
+            destination.append(original[i])
+
+
+    def checkMeaningfulUpdate(self, weights, oldWeights, iterations):
+        if iterations > 200: #TODO: test/change this.
+            return False
+        elif weights == oldWeights:
+            return False
+        else:
+            changeAmount = 0
+            for i in range(0, len(weights)):
+                changeAmount += abs(weights[i] - oldWeights[i])
+            if changeAmount < .01:
+                return False
+            else:
+                return True
+
     def train(self, features, labels):
         """
         :type features: Matrix
@@ -44,31 +63,33 @@ class Perceptron():
         self.labels = []
 
         hasUpdated = True
+        iterations = 0
         while hasUpdated :
-            oldweights = self.weights
-            hasUpdated = 0
+            oldWeights = []
+            self.copyList(self.weights, oldWeights)
             for i in range(features.rows):
-                inputWithBias = features.row(i)
+                inputWithBias = []
+                self.copyList(features.row(i), inputWithBias)
                 inputWithBias.append(1)
                 net = self.computeNet(inputWithBias)
                 output = self.computeOutput(net)
-                testA = labels.row(i)[0]
-                testB = self.type
-                testC = 1 if labels.row(i)[0] else 0
                 self.updateWeights(self.learningRate, 1 if labels.row(i)[0] == self.type else 0, output, inputWithBias)
-                if oldweights == self.weights:
-                    hasUpdate = False
+            hasUpdated = self.checkMeaningfulUpdate(self.weights, oldWeights, iterations) #TODO make better way to check this using accuracy
+            iterations += 1
+        print("number of epochs: " + str(iterations))
 
     def predict(self, features, labels):
         """
         :type features: [float]
         :type labels: [float]
         """
-        net = self.computeNet(features)
+        self.labels = []
+        inputWithBias = []
+        self.copyList(features, inputWithBias)
+        inputWithBias.append(1)
+        net = self.computeNet(inputWithBias)
         output = self.computeOutput(net)
         if output > 0:
-            self.labels.append(0);
-        #    return 1
-        else:
             self.labels.append(1);
-        #    return 0
+        else:
+            self.labels.append(0);
--- custom_manager/toolkit/PerceptronLearner.py
+++ custom_manager/toolkit/PerceptronLearner.py
@@ -5,16 +5,26 @@ from matrix import Matrix
 from Perceptron import Perceptron
 
 class PerceptronLearner(SupervisedLearner):
-    setosaPerceptron = Perceptron(0.0)
-    versicolorPerceptron = Perceptron(1.0)
-    virginicaPerceptron = Perceptron(2.0)
+    #setosaPerceptron = Perceptron(0.0, 4)
+    #versicolorPerceptron = Perceptron(1.0, 4)
+    #virginicaPerceptron = Perceptron(2.0, 4)
+
+    perceptronList = []
+
+    def __init__(self):
+        smallPerceptron = Perceptron(1.0, 2)
+        self.perceptronList.append(smallPerceptron)
+        #setosaPerceptron = Perceptron(0.0, 4)
+        #versicolorPerceptron = Perceptron(1.0, 4)
+        #virginicaPerceptron = Perceptron(2.0, 4)
+        #self.perceptronList.append(setosaPerceptron)
+        #self.perceptronList.append(versicolorPerceptron)
+        #self.perceptronList.append(virginicaPerceptron)
 
     def train(self, features, labels):
 
-        for i in range(0,1):
-            self.setosaPerceptron.train(features, labels)
-            self.versicolorPerceptron.train(features, labels)
-            self.virginicaPerceptron.train(features,labels)
+        for i in range(0,len(self.perceptronList)):
+            self.perceptronList[i].train(features, labels)
 
 
     def predict(self, features, labels):
@@ -22,14 +32,28 @@ class PerceptronLearner(SupervisedLearner):
         :type features: [float]
         :type labels: [float]
         """
-        self.setosaPerceptron.predict(features, labels)
-        self.versicolorPerceptron.predict(features, labels)
-        self.virginicaPerceptron.predict(features, labels)
+        #labels = []
+        for i in range(0,len(self.perceptronList)):
+            self.perceptronList[i].predict(features, labels)
 
-        for index in range(0, len(self.setosaPerceptron.labels)):
-            if(self.setosaPerceptron.labels[index] == 1):
-                labels.append(0.0);
-            elif (self.versicolorPerceptron.labels[index] == 1):
-                labels.append(1.0);
-            elif (self.virginicaPerceptron.labels[index] == 1):
-                labels.append(2.0);
+
+        for i in range(0, len(self.perceptronList)):
+            if self.perceptronList[i].labels[0] == 1:
+                labels.append(1)
+            else:
+                labels.append(0)
+
+        #if self.setosaPerceptron.labels[0] == 1:
+        #    labels.append(0.0)
+        #elif self.versicolorPerceptron.labels[0] == 1:
+        #    labels.append(1.0)
+        #elif self.virginicaPerceptron.labels[0] == 1:
+        #    labels.append(2.0)
+
+        #for index in range(0, len(labels)):
+        #    if(self.setosaPerceptron.labels[index] == 1):
+        #        labels.append(0.0);
+        #    elif (self.versicolorPerceptron.labels[index] == 1):
+        #        labels.append(1.0);
+        #    elif (self.virginicaPerceptron.labels[index] == 1):
+        #        labels.append(2.0);
