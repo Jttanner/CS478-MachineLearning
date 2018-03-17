9f51e0a8af080aea90979ef3eee125f76ece6089
Jttanner <jonttanner@gmail.com>
2018-03-17 11:58:02 -0600

    updates from laptop with broken git

--- custom_manager/toolkit/KNearestNeighbor.py
+++ custom_manager/toolkit/KNearestNeighbor.py
@@ -6,9 +6,8 @@ class DistanceData:
         self.originalIndex = originalIndex
         self.distance = distance
 
-    def __eq__(self, other):  #TODO: confirm it works properly
+    def __eq__(self, other):
         return self.originalIndex == other.originalIndex
-        # return (self.distance == other.distance) and (self.originalIndex == other.originalIndex)
 
 class KNearestNeighbor:
 
@@ -17,24 +16,32 @@ class KNearestNeighbor:
     distancesInOrder = []
     distances = []
 
-    def __init__(self, features, labels):
+    def __init__(self, features, labels, k):
+        self.k = k
         self.features = []
         self.labels = []
         for i in range(features.rows):
             self.features.append(features.row(i))
-            self.labels.append(labels.row(i))
+            self.labels.append(labels.row(i)[0])
         self.distancesInOrder = []
         self.distances = []
 
-    def insertionSortForDistances(self, newEntry, index):
+    def insertForDistances(self, newEntry, index):
         newDistanceData = DistanceData(index, newEntry)
         self.distancesInOrder.append(newDistanceData)
         if len(self.distances) == 0:
             self.distances.append(newDistanceData)
-        else:
-            for i in range(len(self.distances)):
-                if self.distances[i].distance > newDistanceData.distance:
-                    self.distances.append(newDistanceData)
+        else:  #we only care about first k entries
+            if len(self.distances) < self.k:
+                for i in range(len(self.distances)):
+                    if self.distances[i].distance > newDistanceData.distance:
+                        self.distances.insert(i, newDistanceData)
+                    elif i == len(self.distances) - 1:
+                        self.distances.append(newDistanceData)
+            else:
+                for i in range(self.k):
+                    if self.distances[i].distance > newDistanceData.distance:
+                        self.distances.insert(i, newDistanceData)
 
     def calculateDistancesForDataRow(self, row):
         self.distances = []
@@ -42,17 +49,18 @@ class KNearestNeighbor:
         for feature, rowNumber in zip(self.features, range(len(self.features))):  #each entry
             distance = 0
             for i in range(len(row)):  #each attribute
-                distance += (feature[i] - row[i])**2
+                distance += (feature[i] - row[i])**2  #Euclidean
+                #distance += abs(feature[i] - row[i])  #Manhattan
             distance = math.sqrt(distance)
-            self.insertionSortForDistances(distance, rowNumber)
+            self.insertForDistances(distance, rowNumber)
 
-    def nearestNeighborVote(self, k, row):
+    def nearestNeighborVote(self, row):
         votes = []
         for i in range(len(row)):
             votes.append(0)
-        for i in range(k):
+        for i in range(self.k):
             index = self.distances[i].originalIndex
-            votes[self.labels[index]] += 1
+            votes[int(self.labels[index])] += 1
         bestVoteIndex = 0
         for i in range(len(votes)):
             if votes[i] > votes[bestVoteIndex]:
--- custom_manager/toolkit/KnnLearner.py
+++ custom_manager/toolkit/KnnLearner.py
@@ -5,14 +5,22 @@ class InstanceBasedLearner(SupervisedLearner):
 
     knn= None
     k = 3
+    rowsPredicted = 0
 
     def __init__(self):
         pass
 
     def train(self, features, labels):
-        self.knn = KNearestNeighbor(features, labels)
+        self.knn = KNearestNeighbor(features, labels, self.k)
 
     def predict(self, row, labels):
         self.knn.calculateDistancesForDataRow(row)
-        prediction = self.knn.nearestNeighborVote(self.k, row)
+        prediction = self.knn.nearestNeighborVote(row)
         labels.append(prediction)
+        self.rowsPredicted += 1
+
+
+        # No weights magic telescope
+        # Training set accuracy: 1.0
+        # Test set accuracy: 0.7856285628562857
+
